e2d45fc9f808653ecd2864cb7e6f18881953d25c
amaleksei <alekseizhigzhitov@gmail.com>
2018-03-30 23:50:06 +0300

    Fix errors travi-ci

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path');
 
-const env = process.env.NODE_ENV
+const env = process.env.NODE_ENV;
 
 const config = {
-	mode: env || 'development'
-}
+  mode: env || 'development',
+};
 
 module.exports = {
-	entry: './src/javascripts/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: ['babel-loader']
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: ['babel-loader', 'eslint-loader']
-			}
-		]
-	}
+  entry: './src/javascripts/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
 };
