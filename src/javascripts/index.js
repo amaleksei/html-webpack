fd34843a205f7111edb02ce830a4def3d57bb010
amaleksei <alekseizhigzhitov@gmail.com>
2018-05-19 18:46:57 +0300

    Remove unused

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,23 +1,3 @@
-// import _ from 'lodash';
-// import * as url from '../assets/images/test-image.gif';
-
 if (module.hot) {
   module.hot.accept();
 }
-
-
-// const imageComponent = () => {
-//   const imageInsert = document.createElement('IMG');
-//   imageInsert.src = url.default;
-//   imageInsert.setAttribute('width', '400px');
-//   imageInsert.setAttribute('height', '300px');
-//   imageInsert.classList.add('image-insert');
-//   return imageInsert;
-// };
-
-//
-// document.body.appendChild(component());
-//
-// document.body.appendChild(desctiption());
-
-// document.body.appendChild(imageComponent());
