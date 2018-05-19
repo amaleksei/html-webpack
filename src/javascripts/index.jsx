f2a2124e5690be45ae2ac9946181f93e08edcdf6
amaleksei <alekseizhigzhitov@gmail.com>
2018-05-19 17:32:15 +0300

    Rewrite Hello to function

--- src/javascripts/index.jsx
+++ src/javascripts/index.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Hello extends React.Component {
-  render() {
-    return (
-        <div>
-        <div className="hello">
-          <span>Hello webpack!</span>
-        </div>
-        <div className="description">
-          <span>Boilerplate for a Webpack and React</span>
-        </div>
-        <div className="image-insert">
-          <img alt="" src="./assets/images/test-image.gif" />
-        </div>
+function Hello() {
+  return (
+    <div>
+      <div className="hello">
+        <span>Hello webpack!</span>
       </div>
-    )
-  }
+      <div className="description">
+        <span>Boilerplate for a Webpack and React</span>
+      </div>
+      <div className="image-insert">
+        <img alt="" src="./assets/images/test-image.gif" />
+      </div>
+    </div>
+  );
 }
-
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
 
