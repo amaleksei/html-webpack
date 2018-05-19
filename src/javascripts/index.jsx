45c4ad144ad3d3060db80f056cb2ce43d750d631
amaleksei <alekseizhigzhitov@gmail.com>
2018-05-19 17:53:19 +0300

    Replace function Hello to App.js and rename it to App

--- src/javascripts/index.jsx
+++ src/javascripts/index.jsx
@@ -1,23 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-function Hello() {
-  return (
-    <div>
-      <div className="hello">
-        <span>Hello webpack!</span>
-      </div>
-      <div className="description">
-        <span>Boilerplate for a Webpack and React</span>
-      </div>
-      <div className="image-insert">
-        <img alt="" src="./assets/images/test-image.gif" />
-      </div>
-    </div>
-  );
-}
-
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 if (module.hot) {
   module.hot.accept();
