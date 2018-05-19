bd5dc133e6b8cbedd45b65a69f48e8bbf63b861f
amaleksei <alekseizhigzhitov@gmail.com>
2018-05-19 17:53:52 +0300

    Add flowconfig and App.jsx

---
+++ src/javascripts/App.jsx
@@ -0,0 +1,21 @@
+// @flow
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <div className="hello">
+        <span>Hello webpack!</span>
+      </div>
+      <div className="description">
+        <span>Boilerplate for a Webpack and React</span>
+      </div>
+      <div className="image-insert">
+        <img alt="" src="./assets/images/test-image.gif" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
